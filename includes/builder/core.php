9b39f16a0fcf8623bb10a6d0ef4ec3182cb35f93
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-22 00:48:17 +0200

    register custom post type via CustomPostType class

--- includes/builder/core.php
+++ includes/builder/core.php
@@ -1,5 +1,7 @@
 <?php
 
+use JazzMan\Post\CustomPostType;
+
 define( 'ET_BUILDER_ENABLE_BFB_OPTIN_MODAL', false );
 
 
@@ -2678,90 +2680,27 @@ function et_pb_enforce_builder_shortcode( $content ) {
 
 if ( ! function_exists( 'et_pb_register_posttypes' ) ) :
 function et_pb_register_posttypes() {
-	$labels = array(
-		'name'               => esc_html__( 'Projects', 'et_builder' ),
-		'singular_name'      => esc_html__( 'Project', 'et_builder' ),
-		'add_new'            => esc_html__( 'Add New', 'et_builder' ),
-		'add_new_item'       => esc_html__( 'Add New Project', 'et_builder' ),
-		'edit_item'          => esc_html__( 'Edit Project', 'et_builder' ),
-		'new_item'           => esc_html__( 'New Project', 'et_builder' ),
-		'all_items'          => esc_html__( 'All Projects', 'et_builder' ),
-		'view_item'          => esc_html__( 'View Project', 'et_builder' ),
-		'search_items'       => esc_html__( 'Search Projects', 'et_builder' ),
-		'not_found'          => esc_html__( 'Nothing found', 'et_builder' ),
-		'not_found_in_trash' => esc_html__( 'Nothing found in Trash', 'et_builder' ),
-		'parent_item_colon'  => '',
-	);
 
-	$args = array(
-		'labels'             => $labels,
-		'public'             => true,
-		'publicly_queryable' => true,
-		'show_ui'            => true,
-		'can_export'         => true,
-		'show_in_nav_menus'  => true,
-		'query_var'          => true,
-		'has_archive'        => true,
-		'rewrite'            => apply_filters( 'et_project_posttype_rewrite_args', array(
-			'feeds'      => true,
-			'slug'       => 'project',
-			'with_front' => false,
-		) ),
-		'capability_type'    => 'post',
-		'hierarchical'       => false,
-		'menu_position'      => null,
-		'show_in_rest'       => true,
-		'supports'           => array( 'title', 'author', 'editor', 'thumbnail', 'excerpt', 'comments', 'revisions', 'custom-fields' ),
-	);
+	$project = new CustomPostType('project',[
+        'has_archive'        => true,
+        'hierarchical'       => true,
+        'capability_type'    => 'page',
+        'supports'           =>
+            ['title',
+             'author',
+             'editor',
+             'thumbnail',
+             'excerpt',
+             'comments',
+             'revisions',
+             'custom-fields',
+             'page-attributes',
+             'post-formats'
+            ],
+    ]);
+    $project->register_taxonomy('project_category');
+    $project->register_taxonomy('project_tag');
 
-	register_post_type( 'project', apply_filters( 'et_project_posttype_args', $args ) );
-
-	$labels = array(
-		'name'              => esc_html__( 'Project Categories', 'et_builder' ),
-		'singular_name'     => esc_html__( 'Project Category', 'et_builder' ),
-		'search_items'      => esc_html__( 'Search Categories', 'et_builder' ),
-		'all_items'         => esc_html__( 'All Categories', 'et_builder' ),
-		'parent_item'       => esc_html__( 'Parent Category', 'et_builder' ),
-		'parent_item_colon' => esc_html__( 'Parent Category:', 'et_builder' ),
-		'edit_item'         => esc_html__( 'Edit Category', 'et_builder' ),
-		'update_item'       => esc_html__( 'Update Category', 'et_builder' ),
-		'add_new_item'      => esc_html__( 'Add New Category', 'et_builder' ),
-		'new_item_name'     => esc_html__( 'New Category Name', 'et_builder' ),
-		'menu_name'         => esc_html__( 'Categories', 'et_builder' ),
-		'not_found'         => esc_html__( "You currently don't have any project categories.", 'et_builder' ),
-	);
-
-	register_taxonomy( 'project_category', array( 'project' ), array(
-		'hierarchical'      => true,
-		'labels'            => $labels,
-		'show_ui'           => true,
-		'show_admin_column' => true,
-		'query_var'         => true,
-		'show_in_rest'      => true,
-	) );
-
-	$labels = array(
-		'name'              => esc_html__( 'Project Tags', 'et_builder' ),
-		'singular_name'     => esc_html__( 'Project Tag', 'et_builder' ),
-		'search_items'      => esc_html__( 'Search Tags', 'et_builder' ),
-		'all_items'         => esc_html__( 'All Tags', 'et_builder' ),
-		'parent_item'       => esc_html__( 'Parent Tag', 'et_builder' ),
-		'parent_item_colon' => esc_html__( 'Parent Tag:', 'et_builder' ),
-		'edit_item'         => esc_html__( 'Edit Tag', 'et_builder' ),
-		'update_item'       => esc_html__( 'Update Tag', 'et_builder' ),
-		'add_new_item'      => esc_html__( 'Add New Tag', 'et_builder' ),
-		'new_item_name'     => esc_html__( 'New Tag Name', 'et_builder' ),
-		'menu_name'         => esc_html__( 'Tags', 'et_builder' ),
-	);
-
-	register_taxonomy( 'project_tag', array( 'project' ), array(
-		'hierarchical'      => false,
-		'labels'            => $labels,
-		'show_ui'           => true,
-		'show_admin_column' => true,
-		'query_var'         => true,
-		'show_in_rest'      => true,
-	) );
 }
 endif;
 
@@ -2810,13 +2749,13 @@ function et_pb_attempt_memory_limit_increase() {
 		die( wp_json_encode( array(
 			'success' => true,
 		) ) );
-	} else {
-		die( wp_json_encode( array(
-			'error' => true,
-		) ) );
 	}
 
-	die();
+    die( wp_json_encode( array(
+        'error' => true,
+    ) ) );
+
+    die();
 }
 endif;
 
