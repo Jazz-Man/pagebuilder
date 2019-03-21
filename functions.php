614b08427168bc91f6f5161f04f84443bfff0757
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-03-22 00:49:46 +0200

    added post-thumbnails support

--- functions.php
+++ functions.php
@@ -52,6 +52,8 @@ function et_setup_theme() {
 
 	// Declare theme supports
 	add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'html5', ['comment-list', 'comment-form', 'search-form', 'gallery', 'caption']);
 
 	add_theme_support( 'post-formats', array(
 		'video', 'audio', 'quote', 'gallery', 'link'
